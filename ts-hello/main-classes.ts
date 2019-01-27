e1c47acf1109856d42c21463e18c4925faba3fdc
zisan13024 <zisan_cse_mbstu@hotmail.com>
2019-01-27 23:28:57 +0600

    no message

--- ts-hello/main-classes.ts
+++ ts-hello/main-classes.ts
@@ -1,7 +1,15 @@
 
 class Point{
-    x: number;
-    y: number;
+
+    constructor(x?:number, y?:number){ 
+        // this ? is used for making this optional. 
+        //so that we can even use this without sending parameters. 
+        this.x = x;
+        this.y=y;
+    }
+
+    private x: number;
+    private y: number;
 
     draw(){
         console.log('X : ' + this.x + ", Y : " + this.y);
@@ -15,7 +23,9 @@ class Point{
 
 // let point: Point = new Point();
 // we can write like this, or this way
-let point = new Point();
-point.x = 1;
-point.y=2;
+// let point = new Point();
+// point.x = 1;
+// point.y=2;
+let point = new Point(1,2);
+
 point.draw();
