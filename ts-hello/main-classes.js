291a0a842417e5a8ff63a782096910d86ee8c2ff
zisan13024 <zisan_cse_mbstu@hotmail.com>
2019-01-27 23:14:00 +0600

    no message

--- ts-hello/main-classes.js
+++ ts-hello/main-classes.js
@@ -12,4 +12,6 @@ var Point = /** @class */ (function () {
 // let point: Point = new Point();
 // we can write like this, or this way
 var point = new Point();
+point.x = 1;
+point.y = 2;
 point.draw();
--- ts-hello/main-classes.ts
+++ ts-hello/main-classes.ts
@@ -4,7 +4,7 @@ class Point{
     y: number;
 
     draw(){
-        console.log('X : ' + this.x + "Y : " + this.y);
+        console.log('X : ' + this.x + ", Y : " + this.y);
     }
 
     getDistance(anotherPoint:Point){
@@ -16,4 +16,6 @@ class Point{
 // let point: Point = new Point();
 // we can write like this, or this way
 let point = new Point();
+point.x = 1;
+point.y=2;
 point.draw();
