567bf97913a28cf98fdd2a8c60b979f430465079
zisan13024 <zisan_cse_mbstu@hotmail.com>
2019-01-28 22:32:13 +0600

    this.IsSelected = true; this.IsSelected = false;.... instead of these two lines, we can write like this : this.isSelected = !this.isSelected;

---
+++ ts-hello/like.component.ts
@@ -0,0 +1,18 @@
+
+export class LikeComponent{
+    constructor(public likesCount:number,public isSelected:boolean){
+
+    }
+
+    onClick(){
+        if(this.isSelected){
+            this.likesCount--;
+            //this.isSelected = false;
+        }else{
+            this.likesCount++;
+            //this.isSelected = true;
+        }
+
+        this.isSelected = !this.isSelected;
+    }
+}
